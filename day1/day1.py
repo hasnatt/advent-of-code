434e576de177dc8d5a7d7a203740b7c92c23c139
hasnat <hasnatabdul5@gmail.com>
2020-12-01 12:56:53 +0000

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+
+numbers_list = []
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
+    numbers_list = lines
+
+for i in range(0, len(numbers_list)): 
+    numbers_list[i] = int(numbers_list[i]) 
+
+
+print(numbers_list)
+
+for i in range(0, len(numbers_list)):
+    for j in range(i, len(numbers_list)):
+        if(numbers_list[i]+numbers_list[j] == 2020):
+
+            print(f' Line {i}: {numbers_list[i]}')
+            print(f' Line {j}: {numbers_list[j]}')
+
+            print(f' i + j = {numbers_list[i]+numbers_list[j]}')
+            print(f' i * j = {numbers_list[i]*numbers_list[j]}')
