7b5b0be84d7c926750e76ce0d372d5ed6e0b01c3
hasnat <hasnatabdul5@gmail.com>
2020-12-09 23:37:36 +0000

    fintom

--- day4/day4p2.py
+++ day4/day4p2.py
@@ -1,11 +1,17 @@
 import re
 KEYS = {'ecl', 'pid', 'eyr', 'hcl', 'byr','iyr', 'hgt'}
 
+hcl = re.compile(r'^#([0-9a-f]{6})$')
+ecl = re.compile(r'^(amb|blu|brn|gry|grn|hzl|oth)$')
+pid = re.compile(r'^[0-9]{9}$')
+hgt = re.compile(r'^([0-9]{2,3})(cm|in)$')
+
+
 def read_lines():
     return open("input.txt").read().split("\n\n")
 
-def part1():
-    lines = read_lines()
+def part1(lines):
+    part2_list = []
     count = 0 
     for i in range(0,len(lines)):
         passports = lines[i].replace('\n', ' ').split()
@@ -13,40 +19,68 @@ def part1():
 
         if passports.keys() >= KEYS:
             count+=1
-    return(count) 
+            part2_list.append(passports)
+    return(count, part2_list) 
 
 def part2():
-    lines = read_lines()
+    passports = part1()[1]
+    c = 0
+    for passport in passports:
+        has_height = hgt.match(passport['hgt'])
+        incm = re.sub('[0-9]', '', passport['hgt'])
 
 
-    for i in range(0,len(lines)):
-        passports = lines[i].replace('\n', ' ').split()
-        passports = dict([fields.split(':') for fields in passports])
 
-        height = int(re.sub('\D', '', passports['hgt']))  
-        # print(re.sub('[0-9]', '', passports['hgt']))
+        if 1920 <= int(passport['byr']) <= 2002:
+            # passports.remove(passport)
+            
+            if int(passport['iyr']) < 2010 or int(passport['iyr']) > 2020:
+                # passports.remove(passport)
+                
+                if 2020 <= int(passport['eyr']) <= 2030:
+                    # passports.remove(passport)
+                    c+=1
+                    
+        # if not has_height:
+        #     passports.remove(passport)
+        #     continue
+        # elif incm == 'cm' and 150 <= int(passport['hgt'][:-2]) <=193 or\
+        #     incm == 'in' and 59 <= int(passport['hgt'][:-2]) <=76:
+        #     passports.remove(passport)
+        #     continue
+             
 
-        if passports.keys() >= KEYS:
-            if not int(passports['byr']) >= 1920 and int(passports['byr']) <= 2002:
-                return()
-            if not int(passports['iyr']) >= 2020 and int(passports['iyr']) <= 2002:
-                return()
-            if re.sub('[0-9]', '', passports['hgt']) == 'cm':
-                if height < 150 and height > 193:
-                    return()
-            elif re.sub('[0-9]', '', passports['hgt']) == 'in':
-                if height < 59 and height > 76:
-                    return()
+
+
+        # elif re.sub('[0-9]', '', passports['hgt']) == 'cm':
+        #      if height < 150 and height > 193:
+            #         return()
+            # elif re.sub('[0-9]', '', passports['hgt']) == 'in':
+            #     if height < 59 and height > 76:
+            #         return()
+            # else:
+            #     return()
+       
+            # if not hcl.match(passports['hcl']):
+            #     return()
+            # if not ecl.match(passports['ecl']):
+            #     return()
+            # if not pid.match(passports['pid']):
+            #     return()
+        # else:
+        #     c+=1    
+    # return(len(passports)) 
+    return(c)      
             
                 
  
 
 
 
-print(part2())
+# print(part2())
 
 
-# print(part1())
+print(part1(read_lines()[0]))
     
 
   
